93e31ac1785cae7c948788fed768d9b14936bbdd
aespinosav <alonso.espinosa@bristol.ac.uk>
2017-01-31 15:17:45 +0000

    script to generate params now works

--- make_skeleton_cities.jl
+++ make_skeleton_cities.jl
@@ -3,22 +3,23 @@
 
 using TrafficNetworks, SkeletonCities
 
-dir = "./Data"   # Directory to write data files to
+#dir = "./Data"   # Directory to write data files to
 
 cities_per_ensemble = 100
 nodes_per_city = 100
 beta_range = 1:0.1:1.9
 
-current_dir = pwd()
-for b in beta_range:
+#cd("./Data")
+for b in 1.0:0.1:1.9
 
+    current_dir = pwd()
     dir_name = @sprintf "/beta_%1.2f" b
     mkdir(current_dir * dir_name)
 
     cd("."*dir_name)
         for i in 1:cities_per_ensemble
             st = "g"*"%0$(length(digits(cities_per_ensemble)))d"*"_beta_%1.2f"
-            file_name = @eval @sprintf($st, i, b) # nasty wizardry...
+            file_name = @eval @sprintf($st, $i, $b) # nasty wizardry...
             g = beta_skeleton(nodes_per_city, b)
             save_graph(g, file_name*".json")
         end
