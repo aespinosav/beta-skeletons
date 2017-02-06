cca274b29631b7a67fe51490737d9411847e0f67
aespinosav <alonso.espinosa@bristol.ac.uk>
2017-02-06 14:55:27 +0000

    Removed the include statement of the tntp_interaction.jl file. This file is now part of the TrafficNetworks.jl module

--- SkeletonCities.jl
+++ SkeletonCities.jl
@@ -11,6 +11,6 @@ export beta_skeleton, save_graph, load_graph, save_graph_dot, load_tntp_to_dataf
 
 include("dirtySkeleton.jl")
 include("graph_read_write.jl")
-include("tntp_interaction.jl") #Contains functions for loading .tntp files into data frames
+#include("tntp_interaction.jl") #Contains functions for loading .tntp files into data frames
 
 end
