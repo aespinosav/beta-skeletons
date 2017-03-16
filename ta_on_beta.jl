f001d32956e28d79ccf102f201d7b265e1ee58e8
aespinosav <alonso.espinosa@bristol.ac.uk>
2017-03-16 12:17:10 +0000

    Changed a bunch of stuff all around to get this to work. od_pairs are given as tuples now since they play better with concatenation that arrays do... However we still have to be careful that this script can only deal with songle od pair things going on. This needs to be more general before I convert this script into functions and put it into the SkeletonCities module, or its own IO module thing for saving data into a database perhaps?... We'll see what we have time to do...

--- ta_on_beta.jl
+++ ta_on_beta.jl
@@ -21,7 +21,7 @@ function od_pairs(g, N)
             destination = rand(1:num_nodes(g))
         end
 
-        push!(od_pair_array, [origin, destination])
+        push!(od_pair_array, (origin, destination))
     end
 
     od_pair_array
@@ -31,11 +31,12 @@ end
 Computes the cost of a specific flow pattern for linear cost function parameter vectors
 a and b.
 
-flows must be a row  of a data frame obtained with flows_data_frame (or same structure!)
+flows must be a row of a data frame obtained with flows_data_frame (or same structure!)
 that only contains the flows on the edges.
 """
 function tot_cost(rn, flows)
-    q_range = flows[:q]
+    #q_range = flows[:q]
+    flows = flows[2:end]
     f = [flows[i][] for i in 1:length(flows)]
     cost = dot(rn.a, f) + dot(f, diagm(rn.b)*f)
 end
@@ -53,18 +54,20 @@ cd(top_dir)
 dirs_with_graphs = readdir()
 
 #Data frame that contains output data (note that we should have the files..)
-#This is pretty much a db schema!!
+#This is pretty much a db schema!! NOTE: only for SINGLE OD PAIRS
 data_frame = DataFrame(graph_id=Array{UTF8String,1}(),
-                       od=Array{Array{Int64,1},1}(),
-                       β=Array{UTF8String,1}(),
+                       od=Array{Tuple{Int64,Int64},1}(),
+                       β=Array{Float64,1}(),
                        q=Array{Float64,1}(),
                        uecost=Array{Float64,1}(),
                        socost=Array{Float64,1}(),
                        poa=Array{Float64,1}())
 
+#global row
+
 for dir in dirs_with_graphs
     cd(dir)
-    println("In $(dir)...")
+    println("\n\nIn dir:$(dir )...")
     # Each dir corresponds to a value of beta
     # Should follow our naming convetion eg. dirs: 'beta_1.14' files: 'g031_beta_1.23.json'
     β = parse(Float64 ,split(dir, '_')[end])
@@ -112,19 +115,24 @@ for dir in dirs_with_graphs
             sols_so = ta_solve(rn, demand_range, regime="SO")
 
             #Generate data frames for UE and SO equilibria
-            data_ue = flows_data_frame(sols_ue, rn, demand_range)
-            data_so = flows_data_frame(sols_so, rn, demand_range)
+            data_ue = flows_data_frame(sols_ue, demand_range)
+            data_so = flows_data_frame(sols_so, demand_range)
 
             println("Appending to DataFrame...")
             for k in 1:length(demand_range)
 
-                f_ue = data_ue[k, 2:end]
-                f_so = data_so[k, 2:end]
+#               f_ue = data_ue[k, 2:end]
+#               f_so = data_so[k, 2:end]
 
-                cost_ue = tot_cost(rn, f_ue)
-                cost_so = tot_cost(rn, f_so)
+                cost_ue = tot_cost(rn, data_ue[k,:])
+                cost_so = tot_cost(rn, data_so[k,:])
+
+                poa = cost_ue/cost_so
+
                 #rows have the following 'columns':     graphid | od_pair | beta | demand (q) | UE cost | SO cost | PoA
-                row = @data [id, od, β, k, cost_ue, cost_so, cost_ue/cost_so]
+               #println("id\tod\tβ\tq\tCue\tCso\tPoA")
+                println("$id\t$od\t$β\t$k\t$cost_ue\t$cost_so\t$poa")
+                row = data([id; od; β; k; cost_ue; cost_so; cost_ue/cost_so])
                 push!(data_frame, row)
             end
         end
