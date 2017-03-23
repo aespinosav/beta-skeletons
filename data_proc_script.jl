cba0fa263687e56e42fe5f8c0feedb6a6c4852f2
aespinosav <alonso.espinosa@bristol.ac.uk>
2017-03-23 11:50:24 +0000

    Edits to data_proc_script it still doesnt work quite yet, but its getting there

--- data_proc_script.jl
+++ data_proc_script.jl
@@ -5,9 +5,22 @@ data_dir = "~/Documents/phd/code/projects/beta-skeletons/Data"
 data_file = "prelim_data.tsv"
 data =  load_beta_sim_data(data_file)
 
-#Get id strings to tag the networks
-graph_ids = unique(data[:graph_id])
 
+#Get simulation run parameters
+demand_range = length(unique(data[:q]))
+graph_ids = unique(data[:graph_id])
+number_of_graphs = length(graph_ids)
+#This variable is done this way to catch if there are graphs for which STAP was run on a different number of od-pair instances
+nuber_of_od_pairs_per_graph =   begin
+                                    array = Int64[]
+                                    for i in 1:number_of_graphs
+                                        idx_list_1 = [find(x->x==graph_ids[i], data[:graph_id])] 
+                                        for i in graph_ids
+                                            num_of_ods = length(unique(data[idx_list_1][:od]))
+                                            push!(array, num_of_ods)
+                                        end
+                                    end
+                                end
 #STAP has been solved for several, single od-pair, scenarios.
 for id in graph_ids
     indices = find(x -> x==id, data[:graph_id])
