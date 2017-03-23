4b85fb6942f4d33d4dbb9183b371a3ed72af2d72
aespinosav <alonso.espinosa@bristol.ac.uk>
2017-03-23 17:01:48 +0000

    Fixed what I thought was a working file, but it is now working again

--- sim_data_io.jl
+++ sim_data_io.jl
@@ -4,12 +4,10 @@ using DataFrames
 Loads a data file (output from simulation) to a data frame for manipulation and analysis.
 
 Basically this only makes sure that the tuples of the od pairs are read as tuples. Other changes might go in here though
+This whole eval parsing thing is ugly...
 """
 function load_beta_sim_data(filename)
-    data = readtable(filename)
-    
-    map!(x -> eval(parse(x)), data[:od]))
-    data[:od] = DataArray{Tuple{Int64,Int64},1}(data[:od]))
-
+    data = readtable(filename) 
+    data[:od] = map(x -> eval(parse(x)), data[:od])
     data
 end
