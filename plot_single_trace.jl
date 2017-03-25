ac4496de810c6c5ca9691fbc5349f958db4c9a9e
aespinosav <alonso.espinosa@bristol.ac.uk>
2017-03-25 14:44:34 +0000

    Customised the theme to get handles on the plotting properties, this could serve as a base for writing code that interacts with gadfly automatically

--- plot_single_trace.jl
+++ plot_single_trace.jl
@@ -2,8 +2,14 @@
 using TrafficNetworks, SkeletonCities, DataFrames, Gadfly
 
 #load data from location on machine (big data files can take a while... care must be taken with how much memory this will take) 
-data_dir = "/space/ae13414/Data/beta-skeletons"
+current_dir = pwd()
+data_dir = "/space/ae13414/Data/beta-skeleton"
 data_filename = "prelim_data.tsv"
+
+#Where to save the resulting graph
+save_dir = "/space/ae13414/phd/projects/beta-skeletons/writeup"
+
+cd(data_dir)
 data = load_beta_sim_data(data_filename)
 
 #Choose the id for the graph and run we want to plot poa vs demand for...
@@ -29,14 +35,47 @@ ylabel = "PoA"
 title = "N=100, β=$β, od=$od"
 
 #Name of file to save pdf plot as
-target_file_name = id*"od_$(od[1])-$(od[2])_"*".pdf"
+target_file_name = id*"od_$(od[1])-$(od[2])"*".pdf"
+
+#Custom theme for adjusting fontsize and things
+custom_theme = Theme(
+    plot_padding=0.1inch,
+    major_label_font_size=9pt,
+    minor_label_font_size=6pt
+    )
+
+#Configure xticks and yticks
+min_q = 0.0
+max_q = maximum(data_frame_to_plot[:q])
+q_step = data_frame_to_plot[:q][2] - data_frame_to_plot[:q][1]
+
+min_poa = 1.0
+max_poa_data = maximum(data_frame_to_plot[:poa])
+max_poa_theoretical = 4.0/3.0 #For affine cost functions
+
+q_ticks = collect(min_q:20*q_step:max_q) #we are going off the fact that we are plotting 100 q values... 
+
+if abs(max_poa_theoretical - max_poa_data) < 0.1
+    intercept = [max_poa_theoretical]  
+    poa_ticks = collect(1.0:0.1:1.4)
+else
+    intercept = []
+    poa_ticks = collect(1.0:0.01:max_poa_data+0.01)
+end
 
 p = plot(data_frame_to_plot,
          x=:q, 
          y=:poa,
+         yintercept=intercept,
          Geom.line,
+         Geom.hline(color=colorant"black"),
          Guide.Title(title),
-         Guide.Xlabel(xlabel),
-         Guide.Ylabel(ylabel))
+         Guide.XLabel(xlabel),
+         Guide.YLabel(ylabel),
+         Guide.xticks(ticks=q_ticks),
+         Guide.yticks(ticks=poa_ticks),
+         custom_theme)
 
-draw(PDF(target_file_name, 4inch, 3inch), p)
+cd(save_dir)
+draw(PDF(target_file_name, 4inch, 3inch), p) #save pdf graph
+cd(current_dir)
