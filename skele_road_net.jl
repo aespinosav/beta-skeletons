b85b2c114dac966cae3d3db2834c54df87b84628
aespinosav <alonso.espinosa@bristol.ac.uk>
2017-05-12 09:52:40 +0100

    Added function that genereates the whole road network with the perturbed lattice -- beta skeleton thing

--- skele_road_net.jl
+++ skele_road_net.jl
@@ -1,5 +1,13 @@
 using TrafficNetworks
 
+"""
+Generates a β-skeleton with n nodes, for given beta. b_mult
+
+skeleton_road_network(n, beta [, b_mult])
+
+b_mult gives the scaling proportion such that b_mult*a is the maximum
+allowed congestion paraemter 'b' per link.
+"""
 function skeleton_road_network(n, beta, b_mult=1.4)
     g = beta_skeleton(n, beta)
 
@@ -29,4 +37,36 @@ function skeleton_graph_αβ(n::Int, α::Float64, β::Float64)
     n_root = Int(sqrt(n)) #This will give an error if given a number of points 
     points = α_set(n_root, α)
     g = β_skeleton(points, β)
+end
+
+
+"""
+Function that assigns cost parameters a and b (linear cost functions f(x) = a + bx)
+to a graph that has coordinates for the nodes. This is based on euclidean distance.
+
+The way b is assigned leaves much room for improvement.
+
+usage,
+
+    road_network_from_geom_graph(g::Graph, b_min_prop=0.1, b_max_prop=1.4)
+
+"""
+function road_network_from_geom_graph(g::Graph, b_min_prop=0.1, b_max_prop=1.4)
+    a_params = Array{Float64,1}()
+    b_params = Array{Float64,1}()
+
+    for i in 1:num_edges(g)
+
+        dist = norm(g.edges[i].target.pos - g.edges[i].source.pos)
+        a = dist #this can be changed to introduce noise
+
+        b_min = b_min_prop * a
+        b_max = b_max_prop * a
+        interval_lenght = b_max - b_min
+        b = b_min + rand()*interval_length
+
+        push!(a_params, a)
+        push!(b_params, b)
+    end
+    rn = RoadNetwork(g, a_params, b_params)
 end
