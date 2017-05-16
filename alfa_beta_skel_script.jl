1dcba973152fdde438c2b08679fc8191882da5e0
aespinosav <alonso.espinosa@bristol.ac.uk>
2017-05-16 14:50:48 +0100

    Added blurb to be written to the info file in the directory where data is to be stored. Also changed the naming convention of the files. Files written now aslo include all the alfa sets generated separately since the same sets will be used for different beta values to be able to compare how the parameter affects ther topology of for the exact same point set.

--- alfa_beta_skel_script.jl
+++ alfa_beta_skel_script.jl
@@ -30,7 +30,7 @@ staps_to_solve_total = n_nets * staps_to_solve_per_net
 #Write PARAMS file
 
 f = open("run_info.md", "w")
-param_string =
+info_string =
 """
 Simulation run on $writing_dir
 ==============================
@@ -39,14 +39,52 @@ Simulation run on $writing_dir
 β range = $β_range
 q range = $q_range
 
+α instances = $α_instances
+
 Network size             = $n_points
 OD samples (per network) = $od_samples
 Number of networks       = $n_nets
 
 
 Total STAPs to solve (nets * demand_values * od_samples per net) = $staps_to_solve_total
+
+
+Simulation
+----------
+
+For the above choice of parameters this script does the following:
+
+For each value of α an ensemble of $α_insatnces perturbed lattices is generated. (randomness is involved)
+From _each_ of these point sets a β-skeleton is formed for _every_ value of β.
+And for each of these graphs flow function parameters are generated (semi-randomly).
+
+For each unique road network generated this way the STAP is solved for $od_samples randomly selected _single_ OD pairis $(length(q_range)) times, which corresponds to all the demand values in the given demand range.
+
+
+Naming convention and contents of files
+---------------------------------------
+
+File for the α-sets are of the form,
+
+    - set_s000a000.dat
+
+The numbers following the 'a' and the 's' are the velue of α and the set id (number of generated set) respectively. These files are written using 'writedlm' with tabs as separators and the object written is of type Array{Array{Float64,1},1}. i.e. an array of 2d arrays which are the points.
+
+Files for β-seletons (generated from these α-sets) have the form,
+
+    - skel_s000a000b000.json
+
+the numbers after the 's' and 'a' are the same as with the α-set files. The number after the 'b' is the value of β for the skeleton. These files are JSON files that represent the graphs and are written (and can be read) using functions from The SkeletonCities module.
+
+
+The files that contain the parameters of the cost functions are,
+
+    - params_s000a000b000.dat
+
+these files just have an hcat of the a and b parameter vectors. They are written usign writedlm and can be read along with the skeleton files to make a RoadNetwork (from TrafficNetworksw module).
+
 """
-write(f, param_string)
+write(f, info_string)
 close(f)
 
 #Script run
@@ -62,12 +100,12 @@ data_frame = DataFrame(graph_id=Array{UTF8String,1}(),
 for a in α_range
     for na in 1:α_instances
         set = α_set(10, a)
+        writedlm("set_s$(na)_a$(a).dat" , set)
         for b in β_range
             g = β_skeleton(set, b)
-            rn = road_network_from_geom_graph(g)
-            
-            g_id = "s$(na)_a$(a)_b$(b)"
+            g_id = "skel_s$(na)_a$(a)_b$(b)"
 
+            rn = road_network_from_geom_graph(g)
         end
     end
 end
