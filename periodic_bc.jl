19ccd6eecb092d7bb4f163345d08f95cd93383f3
aespinosav <alonso.espinosa@bristol.ac.uk>
2017-08-03 16:40:32 +0100

    Commited files that apparently were not being traked... this does not seem right unless I am missing something obvious

--- periodic_bc.jl
+++ periodic_bc.jl
@@ -1,9 +1,10 @@
 using TrafficNetworks, SkeletonCities
 
 """
-Generates a beta skeleton with periodic boundary conditions
+Generates a beta skeleton with periodic boundary conditions. This is not the most efficient as
+it should only be skeletonising with pairs of nodes that require one of them to be from the centere tile.
 """
-function periodic_net_w_lengths(point_set, β; map_image=true)
+function periodic_net_w_lengths(point_set, β)
     
     N = length(point_set)
 
@@ -14,7 +15,7 @@ function periodic_net_w_lengths(point_set, β; map_image=true)
                          [1 1];
                          [1 0];
                          [1 -1];
-                         [0 1]]
+                         [0 -1]]
     
     mega_set = copy(point_set)
     for i in 1:8
@@ -23,62 +24,55 @@ function periodic_net_w_lengths(point_set, β; map_image=true)
             push!(mega_set, vec(this_tiled_set[k]))
         end
     end
-    #mega_set
-    g = β_skeleton(mega_set, β) #This is not the best way of doing this since it draws edges that are ignored (within the other tiles))
+    g = β_skeleton(mega_set, β) #This is not the best way of doing this...
     
+    #Makes an array of the indices of the edges that matter
     edge_indices = []
     for i in 1:N
         incoming = g.in_edges[g.nodes[i]]
         in_indices = [j.index for j in incoming]
         outgoing = g.out_edges[g.nodes[i]]
         out_indices = [j.index for j in outgoing]
-
         push!(edge_indices, in_indices..., out_indices...)
     end
-    edge_indices = sort(unique(edge_indices))
+    edge_indices = sort(unique(edge_indices)) #we are counting the edges that connect the inner tile twice...
 
-    sources = [g.edges[i].source.index for i in edge_indices]
-    targets = [g.edges[i].target.index for i in edge_indices]
+    #We get the nodes (in order), these are the real nodes, plus the ones that correspond
+    #to edges that cross the borders
+    sources = Array{Int,1}([g.edges[i].source.index for i in edge_indices])
+    targets = Array{Int,1}([g.edges[i].target.index for i in edge_indices])
 
-    edge_lengths = [norm(g.edges[i].target.pos - g.edges[i].source.pos) for i in edge_indices]
-
-    if map_image
-        mapped_sources = map(x -> mod(x, N)+1, sources)
-        mapped_targets = map(x -> mod(x, N)+1, targets)
-        edge_tuples = zip(mapped_sources, mapped_targets)
-
-        g2 = Graph()
-        for i in 1:N
-            add_node!(g2, Node(i, point_set[i]))
-        end
-        for j in edge_tuples
-            connect!(g2, j[1], j[2])
-        end
-    else 
-        available_indices =unique(sort(sources))
-        println("Avail: ", available_indices)
-        extra_indices = available_indices[N+1:end]
+    edge_lengths = Array{Float64,1}([norm(g.edges[i].target.pos - g.edges[i].source.pos) for i in edge_indices])
         
-        edge_tuples = Array{Tuple{Int,Int},1}()
-        for i in 1:length(sources)
-            s = find(x -> x==sources[i], available_indices)[1]
-            t = find(x -> x==targets[i], available_indices)[1]
-            println("($s, $t)")
-            push!(edge_tuples, (s,t))  
+    available_node_indices = unique(sort(sources))
+    extra_indices = available_node_indices[N+1:end]
 
-        end
-         
-        num_of_image_nodes = length(available_indices) - N
+    node_image(x) = Int(x - floor(x/(N+0.01))*N)
+    node_images = Array{Int,1}([node_image(i) for i in available_node_indices])
 
-        g2 = Graph()
-        for i in 1:(N + num_of_image_nodes)
-            add_node!(g2, Node(i, mega_set[available_indices[i]]))
-        end
-        for j in edge_tuples
-            connect!(g2, j[1], j[2])
-        end
+    mapped_sources = Array{Int,1}([node_image(i) for i in sources])
+    mapped_targets = Array{Int,1}([node_image(i) for i in targets])
+
+    edge_tuples = Array{Tuple{Int,Int},1}()
+    for i in 1:length(sources) #because all edges have a source
+        s = find(x -> x==sources[i], available_node_indices)[1]
+        t = find(x -> x==targets[i], available_node_indices)[1]
+        push!(edge_tuples, (s,t))  
     end
-    g2, edge_lengths
+     
+    num_of_image_nodes = length(available_node_indices) - N
+
+    #Construction of graph, nodes get relabeled to have consecutive indices
+    #The information of which node they are an image of is kept in the array node_images
+    g2 = Graph()
+    for i in 1:(N + num_of_image_nodes)
+        add_node!(g2, Node(i, mega_set[available_node_indices[i]]))
+    end
+    for j in edge_tuples
+        connect!(g2, j[1], j[2])
+    end
+
+    g2, edge_lengths, edge_tuples, node_images
 end
 
 
--- plot_graph_tikz.jl
+++ plot_graph_tikz.jl
@@ -1,3 +1,5 @@
+using Colors
+
 """
 Saves a .tex file that contains a tikz image. Currently it generates an article document in LaTeX.
 Usage is as follows:
@@ -13,8 +15,8 @@ If network is truley a directed graph then it MUST be specified.
 The optional variable 'standalone_doc' can be set to true if  the diagram is to be rendered by itself
 as a pdf document.
 """
-function save_graph_tikz(g::Graph, filename::AbstractString; bidirectional=true, standalone_doc=false)
-    
+function save_graph_tikz(g::Graph, filename::AbstractString; bidirectional=true, standalone_doc=true)
+    scale = 10.0
     n = num_nodes(g)
     m = num_edges(g)
     
@@ -30,11 +32,14 @@ function save_graph_tikz(g::Graph, filename::AbstractString; bidirectional=true,
 
     for i in 1:n
         x, y = g.nodes[i].pos
-        str *= "\\coordinate ($i) at ($(x*5.0),$(y*5.0));\n"
+        str *= "\\coordinate ($i) at ($(x*scale),$(y*scale));\n"
     end
 
     str *= "\\end{scope}\n"
-    
+   
+    str *="""\\begin{scope}
+  \\draw[gray,thin,dashed] (0,0) rectangle ($(scale),$(scale));
+  \\end{scope}\n"""
 
     if m != 0   #If there are no edges, adding an empty scope is likely to cause problems
         str *= "\\begin{scope}\n\n"
@@ -54,8 +59,11 @@ function save_graph_tikz(g::Graph, filename::AbstractString; bidirectional=true,
         \\end{tikzpicture}"""
 
     if standalone_doc
-        head = """\\documentclass{minimal}
+        head = """\\documentclass{article}
 \\usepackage{tikz}
+\\usepackage[active,tightpage]{preview}
+\\PreviewBorder=2pt
+\\PreviewEnvironment{tikzpicture}
 \\begin{document}"""
         tail = "\n\\end{document}"
 
@@ -66,6 +74,81 @@ function save_graph_tikz(g::Graph, filename::AbstractString; bidirectional=true,
         write(f, str)
     end
 end
+
+
+function plot_flows_net(filename, g, flows; tol = 1e-4)
+
+    scale = 10.0
+    n = num_nodes(g)
+    m = num_edges(g)
+
+    min_flow = minimum(flows)
+    max_flow = maximum(flows)
+
+    cmap = colormap("Oranges") #Defines 100 colors
+
+    edge_index_step =1
+
+    str="""\\begin{tikzpicture}
+\\begin{scope}
+\\tikzstyle{every node}=[draw=none,fill=none]\n"""
+
+    for i in 1:n
+        x, y = g.nodes[i].pos
+        str *= "\\coordinate ($i) at ($(x*scale),$(y*scale));\n"
+    end
+
+    str *= "\\end{scope}\n"
+   
+    str *="""\\begin{scope}
+  \\draw[gray,thin,dashed] (0,0) rectangle ($(scale),$(scale));
+  \\end{scope}\n"""
+
+    if m != 0   #If there are no edges, adding an empty scope is likely to cause problems
+        str *= "\\begin{scope}\n\n"
+
+        for j in 1:edge_index_step:m
+            value = flows[j]
+            
+            if value > tol
+                edge = g.edges[j]
+                idx = Int(ceil((value / (max_flow+0.0000000000000001))*100))
+                edge_color = cmap[idx]
+                color_str = "{rgb,255:red,$(edge_color.r*255);green,$(edge_color.g*255);blue,$(edge_color.b*255)}"
+
+                s = edge.source.index
+                t = edge.target.index
+
+                str *= "\\draw[->,color=$(color_str),thick] ($s) -- ($t);\n"
+            end
+        end
+
+        str *= "\\end{scope}\n"
+    end
+
+    str*=
+        """
+        \\end{tikzpicture}"""
+
+    head = """\\documentclass{article}
+\\usepackage{tikz}
+\\usepackage[active,tightpage]{preview}
+\\PreviewBorder=2pt
+\\PreviewEnvironment{tikzpicture}
+\\begin{document}"""
+    tail = "\n\\end{document}"
+
+    str = head *str* tail
+
+    open(filename, "w") do f
+        write(f, str)
+    end
+end
+
+
+
+
+
 
 function save_graph_tikz_circ(g::Graph, filename::AbstractString; bidirectional=true, standalone_doc=true)
     
@@ -78,8 +161,8 @@ function save_graph_tikz_circ(g::Graph, filename::AbstractString; bidirectional=
         edge_index_step =1
     end
 
-    str="""\\begin{tikzpicture}[every node/ .style={draw,shape=circle,fill=black,minimum size=3mm,inner sep=0pt,outer sep=0pt}]
-\\begin{scope}\n"""
+    str="""\\begin{tikzpicture}
+\\begin{scope}\n\\tikzstyle{every node}=[draw,circle,fill=black,minimum size=3pt]\n"""
 
     for i in 1:n
         x, y = g.nodes[i].pos
