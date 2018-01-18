c935a3cd3e5e6267131d0d127cfc75c841579ca3
aespinosav <alonso.espinosa@bristol.ac.uk>
2018-01-18 18:52:51 +0000

    Added the functions from tools.file (newly moved here and with the unused bits chopped off from the version in the tgf dir) to the export list

--- SkeletonCities.jl
+++ SkeletonCities.jl
@@ -11,7 +11,11 @@ export beta_skeleton, β_skeleton, save_graph, load_graph,
        load_tntp_to_dataframe, skeleton_road_network,
        load_beta_sim_data, α_set, skeleton_graph_αβ,
        road_network_from_geom_graph, save_graph_tikz,
-       periodic_net_w_lengths, torus_od
+       periodic_net_w_lengths, torus_od,
+       #From tools.jl
+       connect_net!, sample_ensemble, save_net_json,
+       load_net_json, g_lens_for_sim, populate_flows_vis,
+       resource_allocation
 
 include("dirtySkeleton.jl")
 include("skele_road_net.jl")
@@ -20,5 +24,8 @@ include("sim_data_io.jl")
 include("lattice.jl")
 include("periodic_bc.jl")
 include("plot_graph_tikz.jl")
+include("tools.jl") #The functions from this file must be integrated
+#into the corresponding module...
+
 #include("tntp_interaction.jl") #Contains functions for loading .tntp files into data frames
 end
