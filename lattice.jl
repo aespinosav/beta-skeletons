c1a264ba8343f4a1b188c177b1516a67f49e0faa
aespinosav <alonso.espinosa@bristol.ac.uk>
2018-02-06 14:30:48 +0000

    Changed functions in lattice.jl to the new 2D array standard of saving data, some things might cause function interaction to break, I really should have started a new branch before doing this

--- lattice.jl
+++ lattice.jl
@@ -7,34 +7,67 @@ Returns the indices i,j of the position of the k-th point in the lattice
 function indices_of_lattice_point(k, n_root) 
         i = floor(k/n_root) # which row (from top to bottom) 0-start I think
         j = mod(k, n_root)
-
         indices = [Int(i),Int(j)]
 end
 
+#"""
+#Make rectangular lattice of (n_root)^2 points in an (a x a) square.
+#The clearance is the padding between the "outer" points of the lattice
+#and the unit (a) square boundary.
+#"""
+#function square_lattice(n_root, a=1)#; clearance=0.05)
+#    n = n_root^2
+#    clearance = (a/n)*0.5
+#    gap = (a - 2*clearance) / (n_root - 1)
+#    
+#    points = Array{Float64,1}[]
+#    for k in 0:n-1
+#        i, j = indices_of_lattice_point(k, n_root)
+#        p_k = [clearance + (j*gap), clearance + (i*gap)]
+#        push!(points, p_k)
+#    end
+#    points
+#end
 
 """
 Make rectangular lattice of (n_root)^2 points in an (a x a) square.
 The clearance is the padding between the "outer" points of the lattice
 and the unit (a) square boundary.
 """
-function square_lattice(n_root, a=1)#; clearance=0.05)
+function square_lattice(n_root, a=1)
     n = n_root^2
-    clearance = (a/n)*0.5
+    clearance = (a/n)*0.5 
     gap = (a - 2*clearance) / (n_root - 1)
-    
-    points = Array{Float64,1}[]
+   
+    points  = Array{Float64}(n,2)
     for k in 0:n-1
         i, j = indices_of_lattice_point(k, n_root)
         p_k = [clearance + (j*gap), clearance + (i*gap)]
-        push!(points, p_k)
+        points[k+1,:] = p_k
     end
     points
 end
 
+
+#function corners(p, α)
+#    c = Array{Float64,1}[[0.0, 0.0],
+#                         [1.0, 0.0],
+#                         [1.0, 1.0],
+#                         [0.0, 1.0]]
+#
+#    κ = Array{Float64,1}[]
+#    for i in 1:4
+#        κ_i = p + α*(c[i] - p)
+#        push!(κ, κ_i)
+#    end
+#    κ
+#end
+
+
 """
 Returns an array of the corners of the rectangle for which a uniformly
 distributed point will be placed that correspnds to the lattice point p
-once it has been berturbed.
+once it has been perturbed.
 
 Returns an array of points, that are the points in anticlockwise order
 starting at the bottom left corner.
@@ -43,17 +76,16 @@ For α = 0 all the corners coincide with p. For α = 1 the corners coincide
 coincide with the corners of the unit square.
 """
 function corners(p, α)
-    c = Array{Float64,1}[[0.0, 0.0],
-                         [1.0, 0.0],
-                         [1.0, 1.0],
-                         [0.0, 1.0]]
+    c = [0.0  0.0;
+         1.0  0.0;
+         1.0  1.0;
+         0.0  1.0]
 
-    κ = Array{Float64,1}[]
+    k = Array{Float64}(4,2)
     for i in 1:4
-        κ_i = p + α*(c[i] - p)
-        push!(κ, κ_i)
+        k[i,:] = p + α*(c[i,:][:] - p)
     end
-    κ
+    k
 end
 
 """
@@ -62,11 +94,11 @@ points given as the corners array. (if not square it might not stretched a bit)
 
 This should be an Array{Array{Float64,1},1}
 """
-function drop_point(corners)
-    xmin = corners[1][1]
-    xmax = corners[2][1]
-    ymin = corners[1][2]
-    ymax = corners[4][2]
+function drop_point(corner_array)
+    xmin = corner_array[1,1]
+    xmax = corner_array[2,1]
+    ymin = corner_array[1,2]
+    ymax = corner_array[4,2]
 
     xlength = xmax - xmin
     ylength = ymax - ymin
@@ -74,7 +106,7 @@ function drop_point(corners)
     x = rand()*xlength
     y = rand()*ylength
     
-    p = [x,y] + corners[1]
+    p = [x,y] + corner_array[1,:][:]
 end
 
 """
@@ -89,16 +121,15 @@ function α_set(n_root, α)#; clear=0.05)
     lattice_points = square_lattice(n_root)#, clearance=clear)
     
     if α == 0.0
-        return lattice_points
+        points = lattice_points
     else
-        points = Array{Float64,1}[]
-        for p in lattice_points
-            corn = corners(p, α)
-            actual_point = drop_point(corn)
-            push!(points, actual_point)
+        points = Array{Float64}(n,2)
+        for i in 1:n
+            k = corners(lattice_points[i,:][:], α)
+            points[i,:] = drop_point(k)
         end
-        return points    
     end
+    points
 end
 
 """
