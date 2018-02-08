0b5242a68f199d9b2440c19f49d65735f79a8ec9
aespinosav <alonso.espinosa@bristol.ac.uk>
2018-02-08 19:02:15 +0000

    Cleaned up SkeletonCities.jl a bit, am now not importing crap I dont need

--- SkeletonCities.jl
+++ SkeletonCities.jl
@@ -7,15 +7,21 @@ module SkeletonCities
 
 using TrafficNetworks
 
-export β_skeleton, random_beta_skeleton, save_graph, load_graph,
-       load_tntp_to_dataframe, skeleton_road_network,
-       load_beta_sim_data, α_set, skeleton_graph_αβ,
-       road_network_from_geom_graph, save_graph_tikz,
-       periodic_net_w_lengths, torus_od,
-       #From tools.jl
-       connect_net!, sample_ensemble, save_net_json,
-       load_net_json, g_lens_for_sim, populate_flows_vis,
-       resource_allocation, load_road_network
+export 
+    #From dirtySkeleton.jl
+    β_skeleton,
+    #From skele_road_net.jl
+    skeleton_graph_αβ, skeleton_road_network,
+    #From lattice.jl
+    α_set,
+    #From periodic_bc.jl
+    periodic_net_w_lengths, torus_od,
+    #From plot_graph_tikz.jl
+    save_graph_tikz, plot_flows_net,
+    #From tools.jl
+    sample_ensemble, save_net_json,
+    load_net_json, g_lens_for_sim, populate_flows_vis,
+    resource_allocation, load_road_network
 
 include("dirtySkeleton.jl")
 include("skele_road_net.jl")
@@ -24,8 +30,5 @@ include("skele_road_net.jl")
 include("lattice.jl")
 include("periodic_bc.jl")
 include("plot_graph_tikz.jl")
-include("tools.jl") #The functions from this file must be integrated
-#into the corresponding module...
-
-#include("tntp_interaction.jl") #Contains functions for loading .tntp files into data frames
+include("tools.jl") #Having this is messy, must fix!
 end
