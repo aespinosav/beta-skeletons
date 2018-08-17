0142ec38d7a844731476380373168f7729b31f94
aespinosav <alonso.espinosa@gmail.com>
2018-08-17 06:13:41 +0100

    Update of the boundary conditions function for the rectangular array changes. This still needs to be checked further for memory and efficiency optimisation

--- periodic_bc.jl
+++ periodic_bc.jl
@@ -1,10 +1,86 @@
 using TrafficNetworks, SkeletonCities
 
+#"""
+#Generates a beta skeleton with periodic boundary conditions. This is not the most efficient as
+#it should only be skeletonising with pairs of nodes that require one of them to be from the centere tile.
+#"""
+#function periodic_net_w_lengths(point_set::Array{Array{Float64,1},1}, β)
+#    
+#    N = length(point_set)
+#
+#    translation_vects = [-1 -1;
+#                         -1 0;
+#                         -1 1;
+#                          0 1;
+#                          1 1;
+#                          1 0;
+#                          1 -1;
+#                          0 -1]
+#    
+#    mega_set = copy(point_set)
+#    
+#    for i in 1:8
+#        this_tile = broadcast(+, point_set, translation_vects[i,:])
+#        mega_set = vcat(mega_set, this_tile)
+#    end
+#     
+#    g = β_skeleton(mega_set, β) #This is not the best way of doing this...
+#    
+#    #Makes an array of the indices of the edges that matter
+#    edge_indices = []
+#    for i in 1:N
+#        incoming = g.in_edges[g.nodes[i]]
+#        in_indices = [j.index for j in incoming]
+#        outgoing = g.out_edges[g.nodes[i]]
+#        out_indices = [j.index for j in outgoing]
+#        push!(edge_indices, in_indices..., out_indices...)
+#    end
+#    edge_indices = sort(unique(edge_indices)) #we are counting the edges that connect the inner tile twice...
+#
+#    #We get the nodes (in order), these are the real nodes, plus the ones that correspond
+#    #to edges that cross the borders
+#    sources = Array{Int,1}([g.edges[i].source.index for i in edge_indices])
+#    targets = Array{Int,1}([g.edges[i].target.index for i in edge_indices])
+#
+#    edge_lengths = Array{Float64,1}([norm(g.edges[i].target.pos - g.edges[i].source.pos) for i in edge_indices])
+#        
+#    available_node_indices = unique(sort(sources))
+#    extra_indices = available_node_indices[N+1:end]
+#
+#    node_image(x) = Int(x - floor(x/(N+0.01))*N)
+#    node_images = Array{Int,1}([node_image(i) for i in available_node_indices])
+#
+#    mapped_sources = Array{Int,1}([node_image(i) for i in sources])
+#    mapped_targets = Array{Int,1}([node_image(i) for i in targets])
+#
+#    edge_tuples = Array{Tuple{Int,Int},1}()
+#    for i in 1:length(sources) #because all edges have a source
+#        s = find(x -> x==sources[i], available_node_indices)[1]
+#        t = find(x -> x==targets[i], available_node_indices)[1]
+#        push!(edge_tuples, (s,t))  
+#    end
+#     
+#    num_of_image_nodes = length(available_node_indices) - N
+#
+#    #Construction of graph, nodes get relabeled to have consecutive indices
+#    #The information of which node they are an image of is kept in the array node_images
+#    g2 = Graph()
+#    for i in 1:(N + num_of_image_nodes)
+#        add_node!(g2, Node(i, mega_set[available_node_indices[i],:][:]))
+#    end
+#    for j in edge_tuples
+#        connect!(g2, j[1], j[2])
+#    end
+
+#    g2, edge_lengths, edge_tuples, node_images
+#end
+
 """
 Generates a beta skeleton with periodic boundary conditions. This is not the most efficient as
 it should only be skeletonising with pairs of nodes that require one of them to be from the centere tile.
 """
-function periodic_net_w_lengths(point_set, β)
+
+function periodic_net_w_lengths(point_set::Array{Array{Float64,1},1}, β)
     
     N = length(point_set)
 
@@ -20,7 +96,7 @@ function periodic_net_w_lengths(point_set, β)
     mega_set = copy(point_set)
     
     for i in 1:8
-        this_tile = broadcast(+, point_set, translation_vects[i,:])
+        this_tile = [point_set[j] + translation_vects[i,:] for j in 1:N]
         mega_set = vcat(mega_set, this_tile)
     end
      
@@ -66,13 +142,19 @@ function periodic_net_w_lengths(point_set, β)
     #The information of which node they are an image of is kept in the array node_images
     g2 = Graph()
     for i in 1:(N + num_of_image_nodes)
-        add_node!(g2, Node(i, mega_set[available_node_indices[i],:][:]))
+        add_node!(g2, Node(i, mega_set[available_node_indices[i]][:]))
     end
     for j in edge_tuples
-        connect!(g2, j[1], j[2])
+        connect_net!(g2, j[1], j[2])
     end
 
     g2, edge_lengths, edge_tuples, node_images
+end
+
+function periodic_net_w_lengths(point_set::Array{Float64,2}, β)
+    num_points = size(point_set)[1]
+    new_point_set = [point_set[i,:] for i in 1:num_points] #array of arrays
+    periodic_net_w_lengths(new_point_set, β)
 end
 
 
@@ -86,5 +168,22 @@ function torus_od(rn::RoadNetwork)
     o = indmin(norms)
     dists_from_center = [norm(g.nodes[i].pos - [0.5,0.5]) for i in 1:N]
     d = indmin(dists_from_center)
+   
+    od_matrix_from_pair(g, (o,d))
+end
+
+function torus_od₂(rn::RoadNetwork)
+    g = rn.g
+    n = num_nodes(g)
+    dists_from_o =  100*ones(n)
+    dists_from_center = 100*ones(n)
+    for i in 1:n
+      pos = g.nodes[i].pos
+      
+      dists_from_o[i] = norm(pos - [0.5, 0.0])
+      dists_from_center[i] = norm(pos - [1.0,0.5])
+    end
+    o, d = indmin(dists_from_o), indmin(dists_from_center)
+    
     od_matrix_from_pair(g, (o,d))
 end
